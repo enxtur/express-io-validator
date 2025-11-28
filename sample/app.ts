626497dda8838b3b680d6d5e7b34fa45ea9864d5
enxtur <enxtur.mon@gmail.com>
2025-11-28 18:42:30 +0800

    docs: add sample Express application with validation examples
    Add sample application demonstrating validator usage with basic type
    validation and union types. Includes Express server setup with
    custom error handler examples.

---
+++ sample/app.ts
@@ -0,0 +1,65 @@
+import bodyParser from "body-parser";
+import express from "express";
+import * as t from "io-ts";
+import { Validator } from "../src";
+
+const { validate } = Validator((error) => {
+  return {
+    errors: error.flatMap((err) => {
+      return err.context.map((c) => ({
+        key: c.key,
+        expected: c.type?.name,
+        actual: c.actual ?? "undefined",
+        message: "something went wrong",
+      }));
+    }),
+  };
+});
+
+export const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const CODEC = t.type({
+  name: t.string,
+  age: t.number,
+  info: t.type({
+    gender: t.string,
+    email: t.string,
+  }),
+});
+
+app.post(
+  "/users",
+  validate(CODEC, (req, res) => {
+    res.json({
+      name: req.body.name,
+      age: req.body.age,
+      info: {
+        gender: req.body.info.gender,
+        email: req.body.info.email,
+      },
+    });
+  })
+);
+
+const CODEC2 = t.union([
+  t.type({
+    name: t.string,
+    age: t.number,
+  }),
+  t.type({
+    info: t.type({
+      gender: t.string,
+      email: t.string,
+    }),
+  }),
+]);
+
+app.post(
+  "/users2",
+  validate(CODEC2, (req, res) => {
+    res.json(req.body);
+  })
+);
